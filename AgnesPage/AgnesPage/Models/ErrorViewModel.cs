85c4815a58be932aa89e0d7027c4ec0d70c92bcf
agnesrhee <32945942+agnesrhee@users.noreply.github.com>
2024-09-09 20:40:53 -0400

    first commit!

---
+++ AgnesPage/AgnesPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgnesPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
